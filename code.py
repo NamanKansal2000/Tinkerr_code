c3494fea5e5111f8a4a75e61839831461977ce75
NamanKansal2000 <33037182+NamanKansal2000@users.noreply.github.com>
2022-04-30 10:52:07 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,36 @@
+# coded in python
+from collections import Counter
+def func(arr):
+    dict = Counter(arr)
+    count = 0
+    for i in dict:
+        val = dict[i]-1
+        count+=((val*(val+1))//2)
+    return count
+
+arr = [1,2,3,1,2,1]
+print(func(arr))
+
+# time complexity - O(N)
+# Space complexity - O(N)
+
+
+# Q2
+keys = ["idea", "idae", "bsnl", "nsbl", "grasim", "bata"]
+
+def anagram(arr):
+    d = dict()
+    ls = []
+    for i in arr:
+        val = ''.join(sorted(list(i)))
+        if val in d:
+            ls[d[val]].append(i)
+        else:
+            ls.append([i])
+
+            d[val] = len(ls)-1
+    return ls
+
+print(anagram(keys))
+# time complexity - O(nlog(len(word)))
+# Space complexity - O(n)
